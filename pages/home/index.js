69cb2725b60e5b457d91f36023891ce3223fda32
MuhammadHafijur <hafijur.salah@gmail.com>
2022-06-02 09:22:32 +0600

    testing tailwindcss

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-green-500">Hello Homepage</h1>
+      <h1 className="text-indigo-500 text-3xl text-center">Hello Homepage</h1>
     </div>
   );
 };
